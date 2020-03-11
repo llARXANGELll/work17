f55a3b1eb3906720f3c6121c757ea6e8223240e2
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-11 18:43:53 +0300

    Вроде все настроил

---
+++ src/test/java/work17/Main.java
@@ -0,0 +1,52 @@
+package work17;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    private WebDriver webDriver;
+
+    @Before
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+    }
+    @Test
+    public void savkk() {
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        webDriver.get("https://savkk.github.io/selenium-practice/ ");
+        webDriver.findElement(By.id("button")).click();
+        webDriver.findElement(By.id("first")).click();
+        String excellent = webDriver.findElement(By.xpath("//label[.=\"Excellent!\"]")).getText();
+        Assert.assertEquals("Excellent!", excellent);
+        webDriver.findElement(By.className("button-primary")).click();
+        webDriver.findElement(By.linkText("Great! Return to menu")).click();
+        webDriver.findElement(By.id("checkbox")).click();
+        webDriver.findElement(By.id("two")).click();
+        webDriver.findElement(By.id("go")).click();
+        String result = webDriver.findElement(By.xpath("//label[.=\" two\"]")).getText();
+        Assert.assertEquals("two", result);
+        webDriver.findElement(By.id("radio_one")).click();
+        webDriver.findElement(By.id("radio_go")).click();
+        String radio_result = webDriver.findElement(By.xpath("//label[.=\"one\"]")).getText();
+        Assert.assertEquals("one", radio_result);
+        String text = webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]")).getText();
+        Assert.assertEquals("Great! Return to menu", text);
+        webDriver.findElement(By.id("back")).click();
+
+    }
+
+    @After
+    public void driverOut() {
+        webDriver.quit();
+    }
+}
