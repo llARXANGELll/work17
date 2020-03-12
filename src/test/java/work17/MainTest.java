e652d5225501e6a9d3d97b87daa185602fc59b1d
llARXANGELll <aramh@inbox.ru>
2020-03-13 00:04:55 +0300

    Работает до 2 пункта из задания

--- src/test/java/work17/MainTest.java
+++ src/test/java/work17/MainTest.java
@@ -11,7 +11,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
 
 public class MainTest {
