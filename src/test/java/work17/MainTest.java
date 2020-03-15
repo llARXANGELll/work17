c377af2a6e927fe10d71a1421be1b2c140bce8f2
llARXANGELll <aramh@inbox.ru>
2020-03-15 16:54:18 +0300

    ОТредактировал и подправил юзабилити

--- src/test/java/work17/MainTest.java
+++ src/test/java/work17/MainTest.java
@@ -1,7 +1,6 @@
 package work17;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -21,102 +20,102 @@ public class MainTest {
     private WebDriver webDriver;
 
     @BeforeSuite
-    public void setup(){
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
-    }
-    @Test
-    public void savkk() {
         webDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         webDriver.get("https://savkk.github.io/selenium-practice/ ");
-        //строчки выше не коменитровать при запуске 18 задания
+    }
+
+    @Test(enabled = false)
+    public void testWork17() {
         // Задание 17
-//        webDriver.findElement(By.id("button")).click();
-//        webDriver.findElement(By.id("first")).click();
-//        String webElementExcellent = webDriver.findElement(By.xpath("//label[.=\"Excellent!\"]")).getText();
-//        Assert.assertEquals("Excellent!", webElementExcellent);
-//        webDriver.findElement(By.className("button-primary")).click();
-//        webDriver.findElement(By.linkText("Great! Return to menu")).click();
-//        webDriver.findElement(By.id("checkbox")).click();
-//        webDriver.findElement(By.id("two")).click();
-//        webDriver.findElement(By.id("go")).click();
-//        String webElementResult = webDriver.findElement(By.xpath("//label[.=\" two\"]")).getText();
-//        Assert.assertEquals("two", webElementResult);
-//        webDriver.findElement(By.id("radio_one")).click();
-//        webDriver.findElement(By.id("radio_go")).click();
-//        String webElementRadioResult = webDriver.findElement(By.xpath("//label[.=\"one\"]")).getText();
-//        Assert.assertEquals("one", webElementRadioResult);
-//        String elementGetText = webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]")).getText();
-//        Assert.assertEquals("Great! Return to menu", elementGetText);
-//        webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]/a")).click();
-//        webDriver.findElement(By.id("select")).click();
-//        WebElement heroElement = webDriver.findElement(By.name("hero"));
-//        Select heroSelect = new Select(heroElement);
-//        heroSelect.selectByIndex(2);
-//        WebElement languagesElement = webDriver.findElement(By.name("languages"));
-//        Select languagesSelect = new Select(languagesElement);
-//        languagesSelect.selectByIndex(3);
-//        languagesSelect.selectByIndex(2);
-//        languagesSelect.selectByIndex(4);
-//        webDriver.findElement(By.id("go")).click();
-//        String getSelectText = webDriver.findElement(By.xpath("//label[.='C++, C#, Pascal']")).getText();
-//        Assert.assertEquals(getSelectText,"C++, C#, Pascal");
-//        webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]/a")).click();
-//        webDriver.findElement(By.id("form")).click();
-//        WebElement inputFirstName = webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[1]/input"));
-//        inputFirstName.sendKeys("Aram");
-//        WebElement inputLastName = webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[2]/input"));
-//        inputLastName.sendKeys("Хачатурян");
-//        WebElement inputEmail = webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[3]/input"));
-//        inputEmail.sendKeys("ololo@box.ru");
-//        webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[4]/input[2]")).click();
-//        WebElement inputAddress = webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[5]/input"));
-//        inputAddress.sendKeys("ул.Пушкина, дом. Колотушкина где проживают тянки");
-//        webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[6]/input")).sendKeys("C:/Users/Aram/Desktop/Git/work17/.gitignore.");  //Загружаю файл. Указать относительный путь не получилось, по этому указал полный
-//        WebElement inputTellMe  = webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[7]/textarea"));
-//        inputTellMe.sendKeys("Я изучаю автоматизацию");
-//        webDriver.findElement(By.xpath("//form[@id=\"testform\"]/input")).click();
-//        webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]/a")).click();
-//        webDriver.findElement(By.id("iframe")).click();
-//        webDriver.findElements(By.id("code-frame")).size();
-//        webDriver.switchTo().frame(0);
-//        String textCode = webDriver.findElement(By.xpath("//form/label[@id=\"code\"]")).getText().replaceAll("Your code is: ", "");
-//        webDriver.switchTo().parentFrame();
-//        webDriver.findElement(By.xpath("//input[@name='code']")).sendKeys(textCode);
-//        webDriver.findElement(By.xpath("//input[@name='ok']")).click();
-//        webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]/a")).click();
+        webDriver.findElement(By.id("button")).click();
+        webDriver.findElement(By.id("first")).click();
+        String webElementExcellent = webDriver.findElement(By.xpath("//label[.=\"Excellent!\"]")).getText();
+        Assert.assertEquals("Excellent!", webElementExcellent);
+        webDriver.findElement(By.className("button-primary")).click();
+        webDriver.findElement(By.linkText("Great! Return to menu")).click();
+        webDriver.findElement(By.id("checkbox")).click();
+        webDriver.findElement(By.id("two")).click();
+        webDriver.findElement(By.id("go")).click();
+        String webElementResult = webDriver.findElement(By.xpath("//label[.=\" two\"]")).getText();
+        Assert.assertEquals("two", webElementResult);
+        webDriver.findElement(By.id("radio_one")).click();
+        webDriver.findElement(By.id("radio_go")).click();
+        String webElementRadioResult = webDriver.findElement(By.xpath("//label[.=\"one\"]")).getText();
+        Assert.assertEquals("one", webElementRadioResult);
+        String elementGetText = webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]")).getText();
+        Assert.assertEquals("Great! Return to menu", elementGetText);
+        webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]/a")).click();
+        webDriver.findElement(By.id("select")).click();
+        WebElement heroElement = webDriver.findElement(By.name("hero"));
+        Select heroSelect = new Select(heroElement);
+        heroSelect.selectByIndex(2);
+        WebElement languagesElement = webDriver.findElement(By.name("languages"));
+        Select languagesSelect = new Select(languagesElement);
+        languagesSelect.selectByIndex(3);
+        languagesSelect.selectByIndex(2);
+        languagesSelect.selectByIndex(4);
+        webDriver.findElement(By.id("go")).click();
+        String getSelectText = webDriver.findElement(By.xpath("//label[.='C++, C#, Pascal']")).getText();
+        Assert.assertEquals(getSelectText, "C++, C#, Pascal");
+        webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]/a")).click();
+        webDriver.findElement(By.id("form")).click();
+        WebElement inputFirstName = webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[1]/input"));
+        inputFirstName.sendKeys("Aram");
+        WebElement inputLastName = webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[2]/input"));
+        inputLastName.sendKeys("Хачатурян");
+        WebElement inputEmail = webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[3]/input"));
+        inputEmail.sendKeys("ololo@box.ru");
+        webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[4]/input[2]")).click();
+        WebElement inputAddress = webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[5]/input"));
+        inputAddress.sendKeys("ул.Пушкина, дом. Колотушкина где проживают тянки");
+        webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[6]/input")).sendKeys("C:/Users/Aram/Desktop/Git/work17/.gitignore.");  //Загружаю файл. Указать относительный путь не получилось, по этому указал полный
+        WebElement inputTellMe = webDriver.findElement(By.xpath("//form[@id=\"testform\"]/div[7]/textarea"));
+        inputTellMe.sendKeys("Я изучаю автоматизацию");
+        webDriver.findElement(By.xpath("//form[@id=\"testform\"]/input")).click();
+        webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]/a")).click();
+        webDriver.findElement(By.id("iframe")).click();
+        webDriver.findElements(By.id("code-frame")).size();
+        webDriver.switchTo().frame(0);
+        String textCode = webDriver.findElement(By.xpath("//form/label[@id=\"code\"]")).getText().replaceAll("Your code is: ", "");
+        webDriver.switchTo().parentFrame();
+        webDriver.findElement(By.xpath("//input[@name='code']")).sendKeys(textCode);
+        webDriver.findElement(By.xpath("//input[@name='ok']")).click();
+        webDriver.findElement(By.xpath("//label[.=\"Great! Return to menu\"]/a")).click();
+    }
 
-
-        // 18 задание, начало!
-
-//        webDriver.findElement(By.id("alerts")).click();
-//        webDriver.findElement(By.xpath("//button[.='Get password']")).click();
-//        String alertGetPassword = webDriver.switchTo().alert().getText().replaceAll("Your password: ", "");
-//        webDriver.switchTo().alert().accept();
-//        webDriver.findElement(By.xpath("//button[.='Enter password']")).click();
-//        webDriver.switchTo().alert().sendKeys(alertGetPassword);
-//        webDriver.switchTo().alert().accept();
-//        String returnTextGreat = webDriver.findElement(By.xpath("//label")).getText();
-//        Assert.assertEquals(returnTextGreat, "Great!");
-//        webDriver.findElement(By.xpath("//button[3]")).click();
-
-        // Конец 18 задания первой части
-
-        //18 Задание, негативный тест. Закоментировать задание выше
-
-//        webDriver.findElement(By.id("alerts")).click();
-//        webDriver.findElement(By.xpath("//button[.='Get password']")).click();
-//        String alertGetPassword = webDriver.switchTo().alert().getText();
-//        webDriver.switchTo().alert().accept();
-//        webDriver.findElement(By.xpath("//button[.='Enter password']")).click();
-//        webDriver.switchTo().alert().sendKeys(alertGetPassword);
-//        webDriver.switchTo().alert().accept();
-//        Assert.assertEquals(isCheckElemeinGreat(),false);
-
-        // Конец 18 задания с негативным тестом
-
-        // Задание 18, третья часть
-
+    @Test(enabled = false)
+    public void sestWork18Task2() {
+        webDriver.findElement(By.id("alerts")).click();
+        webDriver.findElement(By.xpath("//button[.='Get password']")).click();
+        String alertGetPassword = webDriver.switchTo().alert().getText().replaceAll("Your password: ", "");
+        webDriver.switchTo().alert().accept();
+        webDriver.findElement(By.xpath("//button[.='Enter password']")).click();
+        webDriver.switchTo().alert().sendKeys(alertGetPassword);
+        webDriver.switchTo().alert().accept();
+        String returnTextGreat = webDriver.findElement(By.xpath("//label")).getText();
+        Assert.assertEquals(returnTextGreat, "Great!");
+        webDriver.findElement(By.xpath("//button[3]")).click();
+    }
+    // Конец 18 задания первой части
+    //18 Задание, негативный тест. Закоментировать задание выше
+    @Test(enabled = false)
+    public void testWork18TaskNegativeTest() {
+        webDriver.findElement(By.id("alerts")).click();
+        webDriver.findElement(By.xpath("//button[.='Get password']")).click();
+        String alertGetPassword = webDriver.switchTo().alert().getText();
+        webDriver.switchTo().alert().accept();
+        webDriver.findElement(By.xpath("//button[.='Enter password']")).click();
+        webDriver.switchTo().alert().sendKeys(alertGetPassword);
+        webDriver.switchTo().alert().accept();
+        Assert.assertEquals(isCheckElemeinGreat(), false);
+    }
+    // Конец 18 задания с негативным тестом
+    // Задание 18, третья часть
+    @Test(enabled = true)
+    public void testWork18Task3() {
         webDriver.findElement(By.id("table")).click();
         webDriver.findElement(By.xpath("//tbody/tr[4]/td/input")).click();
         webDriver.findElement(By.xpath("//tbody/tr[5]/td/input")).click();
